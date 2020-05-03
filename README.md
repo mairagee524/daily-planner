4a396ab3129089d079fdb5dbb5c7ab57af8feb2a
mgarcia24 <garciamaira524@yahoo.com>
2020-05-02 19:20:55 -0700

    added code to make calendar functional

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # daily-planner
+
+In this application, the user will be able to organize his/her/their work schedule by adding events to each hourblock. 
+
+The hourblock is a `textarea` where the user will be able to save it using the button to the right.
+
+There are three colors to represent the past, present, and future. 
+
+Note - this is a work in progress. To make this application fully functional, I need to add/modify code on local storage. 
--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// if current time is less than 9 am, the box will be green and available to write on 
-
-// if current time is equal to 9 am the box will be orange
-
-// how to compare 9 am to 3 pm (military time)
-
-// if else statements 
-
-// time as JS objects
-
-// get webpage populated 
-
-// event i time
-
 // Today's Date
 var dayAndTime = moment().format('dddd, MMMM Do YYYY');
 
@@ -19,8 +5,6 @@ var dayAndTime = moment().format('dddd, MMMM Do YYYY');
 $('#currentDay').append(dayAndTime);
 
 var currentTime = moment().format('H');
-
-
 
 // List of events that user will input
 var appts = [];
@@ -38,22 +22,16 @@ for (var i = 0; i < events.length; i++) {
     timeCol.attr("class", "hour");
     timeCol.width(100);
 
-
-    var eventCol = $('<td>');
+    var eventCol = $('<textarea>');
+    eventCol.attr("class", "textarea");
     eventCol.width(800);
-    eventCol.attr("data-userInput", "");
-    
-    var inputTableCell = $('<input id="user">');
-     console.log(inputTableCell);
-
-    eventCol.append(inputTableCell);
     
     var saveCol = $('<td>');
     var saveButton = $('<button>');
+    saveButton.attr("id", "submit");
     saveButton.attr("class", "saveBtn");
-    saveButton.width(100);
+    saveButton.text("Save");
     saveCol.append(saveButton);
-
 
     tableRow.append(timeCol);
     tableRow.append(eventCol);
@@ -67,73 +45,44 @@ for (var i = 0; i < events.length; i++) {
     if (oneEvent < currentTime) {
         eventCol.attr("class", "past");
 
-    } else if (oneEvent = currentTime) {
-        eventCol.attr("class", "present");
-
-    } else if (oneEvent > currentTime) {
+        } else if (oneEvent > currentTime) {
         eventCol.attr("class", "future");
 
-    } 
+        } else {
+        eventCol.attr("class", "present");
+
+    }; 
 };
 
-saveButton.on('click', 'button', function(event){
+function showSavedEvents() {
+    typedEvents = localStorage.getItem(eventCol[0].value);
+
+    typedEvents = JSON.parse(typedEvents);
+
+    appts.push(typedEvents);
+    };
+
+showSavedEvents(); 
+
+$("submit").on('click', 'button', function(event){
     event.preventDefault();
 
-    submitEvent = $('#user')[0].value;
+    oneEvent = eventCol[0].value;
 
-    console.log($('#user')[0].value);
-
-    appts.push(submitEvent);
-
-    console.log(appts);
-
+    localStorage.setItem("typedEvents", JSON.stringify(oneEvent)); 
+    showSavedEvents();
 });
 
-function numberInColumn(eventTime) {
-    if (eventTime < 12) {
-            beforeNoon = eventTime;
-            timeCol.text(beforeNoon);
-                
-            } else if (eventTime > 12) {
-            afternoonTime = eventTime - 12;
-            timeCol.text(afternoonTime);
+function numberInColumn(currentTime) {
+    if (currentTime < 12) {
+        beforeNoon = currentTime;
+        timeCol.text(beforeNoon);
             
-            } else {
-            timeCol.text(eventTime);
+        } else if (currentTime > 12) {
+        afternoonTime = currentTime - 12;
+        timeCol.text(afternoonTime);
             
-            }
-
+        } else {
+        timeCol.text(currentTime);
+    }
 };
-
-
-// # 05 Third-Party APIs: Work Day Scheduler
-
-// Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-// You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-// ## User Story
-
-// ```
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-// ```
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
